7b53c82177eeb76570003c2d1c832eb90b733a16
bjon13301 <73957816+bjon13301@users.noreply.github.com>
2022-06-17 12:38:50 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,3 +5,8 @@ resource "azurerm_resource_group" "myrg" {
   name     = "Jon-Test-Cloud"
   location = "westus3"
 }
+
+resource "azurerm_resource_group" "myrg2" {
+  name     = "Jon-Test-Cloud2"
+  location = "westus2"
+}
